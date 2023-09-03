464897764df607a91f00da947d43604d2c084b4e
samuel.abreu <samuel.abreu@fleetcor.com.br>
2023-09-03 15:41:49 -0300

    feat: config files

---
+++ config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+var (
+	db     *gorm.DB
+	logger *Logger
+)
+
+func Init() error {
+
+	var err error
+
+	//Initialize SQLite
+	db, err = InitialzeSqlite()
+
+	if err != nil {
+		return fmt.Errorf("error initializing sqlite: %v", err)
+	}
+
+	return nil
+}
+
+func GetSQLiteDB() *gorm.DB {
+	return db
+}
+
+func GetLogger(p string) *Logger {
+	//Initialize Logger
+
+	logger = NewLogger(p)
+	return logger
+}
